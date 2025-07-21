f8d13f891710c39bbe70ec0d98a9910732deb347
PC-LUIS <PC-LUIS@luis>
2025-07-21 11:08:18 -0500

    Entrega final de Modulos

--- views/formulario/listar.php
+++ views/formulario/listar.php
@@ -1,6 +1,7 @@
 <?php $titulo = 'Listado de Formularios'; ?>
 <?php include __DIR__ . '/header_formulario.php'; ?>
 <!--INCLUIR SIEMPRE PARA EL MENU LATERAL-->
+<!--INCLUIR SIEMPRE PARA EL MENU LATERAL-->
 <?php include __DIR__ . '/../../components/sidebar.php'; ?>
 <div class="p-4 w-100">
   <h1 class="h4"> Formularios</h1>
